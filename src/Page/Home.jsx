61d34107a4d52b4138a0cbd16242c575d26c2de1
Facundo Ayosa <ayosafacundo@gmail.com>
2023-03-20 19:55:33 -0300

    home buttons fixed

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,15 +3,15 @@ import WebDev from '../resources/images/Software-Developer.svg';
 
 function Home() {
     const handleClick = () => {
-        console.log("Clicked!")
+
     }
     return (
         <section id="home">
             <img src={WebDev} alt="Web Developer Illustration" />
             <h1 className='title'><span className='whiteText'>Hey!</span> I am a <br /> <span className='rainbow_anim'>Front End Web Developer</span></h1>
             <div className='btns'>
-                <button onClick={handleClick} className="contact">Contact Me</button>
-                <button onClick={handleClick} className="projects">Projects</button>
+                <a href='#contact' className="button contact">Contact Me</a>
+                <a href='#portfolio' className="button projects">Projects</a>
             </div>
         </section>
     )
